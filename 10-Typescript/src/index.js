04e677d8e9472a3debde812687919a2c292684dc
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-10 14:22:59 -0300

    transpilar ts js

---
+++ 10-Typescript/src/index.js
@@ -0,0 +1,6 @@
+// TS deve ser tipado, se não for tipado ele primeiro dá um tipo automático, mas por boas práticas é melhor declarar
+function main() {
+    var myEmail = "brusife@gmail.com";
+    console.log(myEmail);
+}
+main();
---
+++ 10-Typescript/src/index.ts
@@ -0,0 +1,8 @@
+// TS deve ser tipado, se não for tipado ele primeiro dá um tipo automático, mas por boas práticas é melhor declarar
+
+function main() {
+    let myEmail: string = "brusife@gmail.com";
+    console.log(myEmail);
+}
+
+main();
