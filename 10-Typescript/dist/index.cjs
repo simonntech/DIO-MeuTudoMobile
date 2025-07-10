20f1fa5f5792147fb5a818819c2017d3f32f865a
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-10 15:05:25 -0300

    tsup

---
+++ 10-Typescript/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let myEmail = "brusife@mail.com";
+  console.log(myEmail);
+}
+main();
--- 10-Typescript/src/index.ts
+++ 10-Typescript/src/index.ts
@@ -1,7 +1,7 @@
 // TS deve ser tipado, se não for tipado ele primeiro dá um tipo automático, mas por boas práticas é melhor declarar
 
 function main() {
-    let myEmail: string = "brusife@gmail.com";
+    let myEmail: string = "brusife@mail.com";
     console.log(myEmail);
 }
 
