005a67f6d69befa879d2e01546112170d8080bb7
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-10 14:51:08 -0300

    tsconfig

--- 10-Typescript/dist/index.js
+++ 10-Typescript/dist/index.js
@@ -1,6 +1,7 @@
+"use strict";
 // TS deve ser tipado, se não for tipado ele primeiro dá um tipo automático, mas por boas práticas é melhor declarar
 function main() {
-    var myEmail = "brusife@gmail.com";
+    let myEmail = "brusife@gmail.com";
     console.log(myEmail);
 }
 main();
