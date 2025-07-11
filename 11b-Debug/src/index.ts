53df3ecae5e3c4cb8cb6b85b2e74cd506e97da91
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-07-11 10:27:10 -0300

    debug watch

--- 11b-Debug/src/index.ts
+++ 11b-Debug/src/index.ts
@@ -1,3 +1,6 @@
 import { getBaseEmail } from "./services/email"; // no TYPESCRIPT não é preciso passar a extensão
 
 getBaseEmail("mariana")
+
+console.log("Finalizado")
+console.log("...")
--- 11b-Debug/src/services/email.ts
+++ 11b-Debug/src/services/email.ts
@@ -1,11 +1,16 @@
 // a função também pode ser tipada :Promise<"tipo">
 
 async function getBaseEmail(senderName: string): Promise<string> {
-    let base = `Olá ${senderName}, gostaria de me inscrever para uma vaga`;
+    let base = await getHeaderText();
 
+    base += `Olá ${senderName}, gostaria de me inscrever para uma vaga`;
     base += "\n estou deixando o currículo";
 
     return base;
+}
+
+async function getHeaderText(): Promise<string> {
+    return "Email para você"
 }
 
 export { getBaseEmail };
